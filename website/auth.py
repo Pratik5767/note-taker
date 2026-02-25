1a28b2e9a1d7fdee53791e28ec67715bc6c35f3e
Pratik5676 <pratikvsalunkhe924@gmail.com>
2026-02-25 20:09:21 +0530

    user account creation functionality done

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,8 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
+
 
 auth = Blueprint('auth', __name__)
 
@@ -16,21 +20,24 @@ def logout():
 def signup():
     if request.method == 'POST':
         email = request.form.get("email")
-        firstName = request.form.get("firstName")
+        first_name = request.form.get("firstName")
         password = request.form.get('password1')
         confirm_password = request.form.get('password2')
 
         if len(email) < 4:
             flash('Email must be greater than 3 characters.', category='error')
-        elif len(firstName) < 2:
+        elif len(first_name) < 2:
             flash('First name must be greater than 1 characters.', category='error')
         elif password != confirm_password:
             flash('Passwords don\'t match.', category='error')
         elif len(password) < 7:
             flash('Password must be atleast 7 characters.', category='error')
         else:
-            # add user to database
+            new_user = User(email=email, first_name=first_name, password=generate_password_hash(password, method='pbkdf2:sha256'))
+            db.session.add(new_user)
+            db.session.commit()
             flash("Account created!", category='success')
+            return redirect(url_for('views.home'))
 
     return render_template("sign-up.html")
 
