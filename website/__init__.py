baf485495aaee2bb1fd144219b46055c4bbc85c8
Pratik5676 <pratikvsalunkhe924@gmail.com>
2026-02-28 14:05:28 +0530

    authenticating routes

--- website/__init__.py
+++ website/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import configparser
+from flask_login import LoginManager
+
 
 db = SQLAlchemy()
 
@@ -28,6 +30,14 @@ def create_app():
 
     from .models import User, Note
     create_database(app)
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
 
     return app
 
